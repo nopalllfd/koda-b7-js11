cbf76170b30b216ac0423355659c44dd738db98e
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-02-27 16:25:02 +0700

    Refactor promise handling for user names

--- minitask-11.js
+++ minitask-11.js
@@ -1,63 +1,66 @@
 const showName = (username) => {
-  return `${username}`;
+  `${username}`;
 };
 
-const panggilJohn = new Promise((resolve, reject) => {
-  const success = true;
-  if (success) {
-    resolve(showName('John'));
-  } else {
-    reject('Error');
-  }
-});
-const panggilEd = new Promise((resolve, reject) => {
-  const success = true;
-  if (success) {
-    resolve(showName('Ed'));
-  } else {
-    reject('Error');
-  }
-});
-const panggilJane = new Promise((resolve, reject) => {
-  const success = true;
-  if (success) {
-    resolve(showName('Jane'));
-  } else {
-    reject('Error');
-  }
-});
-
-panggilJohn
-  .then((data) => {
-    return new Promise((resolve) => {
+const panggilJohn = () =>
+  new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
       setTimeout(() => {
-        console.log(data);
+        console.log('John');
         resolve();
       }, 1500);
-    });
-  })
-  .then(() => {
-    return panggilEd;
-  })
-  .then((data) => {
-    return new Promise((resolve) => {
+    } else {
+      reject('Error');
+    }
+  });
+
+const panggilEd = () =>
+  new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
       setTimeout(() => {
-        console.log(data);
+        console.log('Ed');
         resolve();
       }, 2000);
-    });
-  })
-  .then(() => {
-    return panggilJane;
-  })
-  .then((data) => {
-    return new Promise((resolve) => {
+    } else {
+      reject('Error');
+    }
+  });
+
+const panggilJane = () =>
+  new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
       setTimeout(() => {
-        console.log(data);
+        console.log('Jane');
         resolve();
       }, 500);
-    });
-  })
+    } else {
+      reject('Error');
+    }
+  });
+
+panggilJohn()
+  .then(() => panggilEd())
+  .then(() => panggilJane())
   .catch((error) => {
     console.log(error);
   });
+
+const panggil = (username, durasi) => {
+  return new Promise((resolve) => setTimeout(() => resolve(`${username}`), durasi));
+};
+
+const panggilNama = async (john, ed, jane) => {
+  try {
+    const panggilJohn = await panggil(john, 1000);
+    console.log(panggilJohn);
+    const panggilEd = await panggil(ed, 2000);
+    console.log(panggilEd);
+    const panggilJane = await panggil(jane, 500);
+    console.log(panggilJane);
+  } catch (error) {}
+};
+
+panggilNama('john', 'ed', 'jane');
